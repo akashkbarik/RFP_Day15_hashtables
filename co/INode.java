f7938b07853448338521089da20be50a707695dc
akashkbarik <akashkbarik12@gmail.com>
2023-03-23 01:24:19 +0530

    Added: created binary search tree using size method

---
+++ co/INode.java
@@ -0,0 +1,14 @@
+package co;
+
+    public class INode<K extends Comparable<K>> {
+        K key;
+        INode left;
+        INode right;
+
+
+        public INode(K key) {
+            this.key = key;
+            this.left = this.right = null;
+        }
+    }
+
